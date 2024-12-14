6f13da4d7634e90ee1cdae480b6d7ad6431d4afc
joshuadinham <joshua-dinham@hotmail.com>
2024-12-14 00:32:00 -0500

    complete tideman

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -31,6 +32,9 @@ void add_pairs(void);
 void sort_pairs(void);
 void lock_pairs(void);
 void print_winner(void);
+void initialize_disjoint_sets(int parent[], int rank[], int candidate_cnt);
+bool union_sets(int parent[], int rank[], int x, int y);
+int find(int parent[], int x);
 
 int main(int argc, string argv[])
 {
@@ -98,41 +102,254 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    //loop through candidates array
+        //compare candidates[i] with given name
+            //if matches
+                //insert candidates[i] into ranks[rank]
+            //return true
+    //end loop
+    //return false
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(candidates[i], name) == 0)
+        {
+            ranks[rank] = i;
+            return true;
+        }
+    }
     return false;
 }
 
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
-    return;
+    //loop over ranks using variable i
+        //the value of ranks[i] represent the candidate itself
+        //the i represents the ranking
+        //loop over ranks again using variable j = i + 1
+            //ranks[i] is preferred over ranks[j]
+            //ranks[j] represents the candidates that ranks[i] is valued over
+            //increment preferences[ranks[i]][ranks[j]]
+        //end loop when j + 1 < length of ranks is false
+    //end loop when i < length of ranks - 1 is false
+
+    for (int i = 0; i < candidate_count - 1; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            preferences[ranks[i]][ranks[j]]++;
+        }
+    }
 }
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
-    return;
+    //loop through the amount of candidates with variable i
+        //loop through amount of candidates with variable j = i + 1
+            //compare preferences[i][j] and preferences[j][i]
+                //select the value which is greater
+                // if preferences[i][j] > preferences[j][i]
+                    //pairs winner = i loser = j
+                // else if preferences[i][j] < preferences[j][i]
+                    //pairs winner = j loser  = i
+            //pairs[pair_count] = pair
+            //++pair_count
+
+    for (int i = 0; i < candidate_count - 1; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                ++pair_count;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                ++pair_count;
+            }
+
+        }
+    }
 }
 
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
-    return;
+    //access the value of the multi-dimensional preferences array using winner and loser of pair
+    //strength = preferences[pairs.winner][pairs.loser]
+    //sorting can be achieved through insertion sort giving array and size of array
+        // loop through the array of pairs using variable i
+            //loop through array of pairs again using variable j = i + 1 decrementing j until 0
+                //set curr to equal to preferences[pairs[j-1].winner][pairs[j-1].loser]
+                //set next = preferences[pairs[j].winner][pairs[j].loser]
+                // if next is greater than curr
+                    //temp = curr
+                    //pairs[i] = next
+                    //pairs[j] = temp
+                //else
+                    //break
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            int curr = preferences[pairs[j-1].winner][pairs[j-1].loser];
+            int next = preferences[pairs[j].winner][pairs[j].loser];
+
+            if (next > curr)
+            {
+               pair temp = pairs[j-1];
+               pairs[j-1] = pairs[j];
+               pairs[j] = temp;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 }
 
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
-    return;
+
+    int i_candidates[candidate_count];
+    int ranks[candidate_count];
+    initialize_disjoint_sets(i_candidates, ranks, candidate_count);
+
+
+    //Assuming edges is an array of structs {int u, int v, int weight}
+    for (int i = 0; i < pair_count; i++) {
+        int u = pairs[i].winner;
+        int v = pairs[i].loser;
+
+        // Try to union the two candidates
+        if (union_sets(i_candidates, ranks, u, v)) {
+            // Successfully added the edge to MST
+            locked[u][v] = true;
+        }
+    }
+
 }
 
 // Print the winner of the election
 void print_winner(void)
 {
-    // TODO
-    return;
+    // Iterate Through Candidates:
+    //For each candidate i, check if locked[j][i] = 0 for all j (where j ≠ i).
+    // Return the Winner: If a candidate meets this condition, they are the winner.
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        int found = 0;
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (j != i)
+            {
+                if (locked[j][i] == true)
+                {
+                    found++;
+                }
+            }
+        }
+        if (found == 0)
+        {
+            printf("%s\n", candidates[i]);
+            break;
+        }
+    }
 }
+
+
+void initialize_disjoint_sets(int parent[], int rank[], int candidate_cnt)
+    // iterate for i such that i < candidate_count
+        //set parent[i] = i
+        //set rank[i] = 0
+    // exit loop
+{
+    for (int i = 0; i < candidate_cnt; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+bool union_sets(int parent[], int rank[], int x, int y)
+    //set root_x to result of find(parent, x);
+    //set root_y to result of find(parent, y);
+
+    //if root_x != root_y
+        //attach the smaller tree under the larger tree
+        // if rank[root_x] > rank[root_y]
+            //attach parent[root_y] = root_x
+        //else if rank[root_y] > rank[root_x]
+            //attach parent[root_x] = root_y
+        //else
+            //if ranks are equal, choose one as root and increment its rank
+            //parent[root_y] = root_x
+            //rank[root_x]++;
+{
+    int root_x = find(parent, x);
+    int root_y = find(parent, y);
+
+    if (root_x != root_y)
+    {
+        if (rank[root_x] > rank[root_y])
+        {
+            parent[root_y] = root_x;
+        }
+        else if (rank[root_y] > rank[root_x])
+        {
+            parent[root_x] = root_y;
+        }
+        else
+        {
+            parent[root_y] = root_x;
+            rank[root_x]++;
+        }
+
+        return true;
+    }
+    return false;
+}
+
+int find(int parent[], int x)
+    //if parent[x] != x
+        //set parent[x] to the return value of find giving parent, and parent[x]
+
+    //return parent[x]
+{
+    if (parent[x] != x)
+    {
+        parent[x] = find(parent, parent[x]);
+    }
+
+    return parent[x];
+}
+
+// int find(int parent[], int x)
+    //if parent[x] != x
+        //set parent[x] to the return value of find giving parent, and parent[x]
+
+    //return parent[x]
+
+//void union_sets(int parent[], int rank[], int x, int y)
+    //set root_x to result of find(parent, x);
+    //set root_y to result of find(parent, y);
+
+    //if root_x != root_y
+        //attach the smaller tree under the larger tree
+        // if rank[root_x] > rank[root_y]
+            //attach parent[root_y] = root_x
+        //else if rank[root_y] > rank[root_x]
+            //attach parent[root_x] = root_y
+        //else
+            //if ranks are equal, choose one as root and increment its rank
+            //parent[root_y] = root_x
+            //rank[root_x]++;
